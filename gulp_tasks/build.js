d559915911b6511ab6a1e217e87bc3c8c472cd57
Denison Martins <denisongmartins@gmail.com>
2017-12-29 11:22:34 -0200

    Added new gulp task

--- gulp_tasks/build.js
+++ gulp_tasks/build.js
@@ -1,5 +1,5 @@
 const runSequence = require('run-sequence');
 
 module.exports = function($, callback) {
-  return runSequence('clean:build', ['txt:copy', 'php:copy', 'scss:compile', 'scss:minify', 'js:copy', 'js:minify', 'pot:compile']);
+  return runSequence('clean:build', ['txt:copy', 'php:copy', 'scss:compile', 'scss:minify', 'js:copy', 'js:minify', 'pot:compile', 'pot:copy']);
 };
