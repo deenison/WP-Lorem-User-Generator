1f8a810c57d69445b8ea6e182e416b28f1ed315c
Denison Martins <denisongmartins@gmail.com>
2018-01-24 22:08:08 -0200

    Bump new version

--- src/lorem-user-generator.php
+++ src/lorem-user-generator.php
@@ -5,7 +5,7 @@
  * Plugin URI: https://github.com/deenison/wp-lorem-user-generator
  * Author: Denison Martins
  * Author URI: https://github.com/deenison
- * Version: 1.1.0
+ * Version: 1.2.0
  * Text Domain: lorem-user-generator
  * Domain Path: /languages
  */
@@ -18,7 +18,7 @@ if (!defined('ABSPATH')) exit;
  *
  * @since   1.0.0
  */
-define('LUG_VERSION', '1.1.0');
+define('LUG_VERSION', '1.2.0');
 
 /**
  * Plugin slug.
