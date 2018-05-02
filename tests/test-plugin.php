7058e90310f530e8318a7f08e64630ff38770103
Denison Martins <denisongmartins@gmail.com>
2018-05-02 11:36:03 -0300

    Fix const

--- tests/test-plugin.php
+++ tests/test-plugin.php
@@ -5,6 +5,6 @@ class PluginTest extends WP_UnitTestCase
 {
     public function testPluginActivation()
     {
-        $this->assertTrue(is_plugin_active(PLUGIN_PATH));
+        $this->assertTrue(is_plugin_active(LUG_PATH));
     }
 }
