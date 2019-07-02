855e9aeedaa71a6da4a1f24b06457867128f27a3
Denison Martins <contact@denison.me>
2019-07-01 21:15:08 -0300

    lorem

---
+++ src/LoremUserGenerator/Foo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace LoremUserGenerator;
+
+class Foo {
+
+    public static function sum(int $a, int $b): int {
+        return $a + $b;
+    }
+}
--- tests/helper.php
+++
@@ -1,5 +0,0 @@
-<?php
-function _manually_load_plugin()
-{
-    require dirname(__DIR__) . '/src/' . LUG_SLUG . '.php';
-}
