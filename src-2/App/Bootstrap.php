ac281bdf9cf26d49f1dc2ed4f6b051b24b2b37ac
Denison Martins <denisongmartins@gmail.com>
2020-06-09 00:25:17 -0300

    Clean up a bit

--- src-2/App/Bootstrap.php
+++ src-2/App/Bootstrap.php
@@ -15,23 +15,9 @@ final class Bootstrap
 
     public static function start(): void
     {
-//        add_action('admin_menu', array(self::class, 'registerAdminMenus'));
-//        add_action('admin_init', array(self::class, 'registerSettings'));
         SettingsController::register();
         AddMultipleUsersController::register();
         NewUserController::register();
         UsersController::register();
-    }
-
-    public static function registerAdminMenus(): void
-    {
-        add_submenu_page(
-            'users.php',
-            'Lorem User Generator',
-            'Lorem User Generator',
-            'create_users',
-            'lorem-user-generator',
-            [NewUserController::class, 'render']
-        );
     }
 }
